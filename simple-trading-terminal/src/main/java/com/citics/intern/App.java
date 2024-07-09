b1d41fcf860dcf8a089704f2b54ea8534ccf1dcc
justinli500 <lijustin500@gmail.com>
2024-07-09 16:20:10 +0800

    Fixed data path issue

--- simple-trading-terminal/src/main/java/com/citics/intern/App.java
+++ simple-trading-terminal/src/main/java/com/citics/intern/App.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Hello world!
  *
@@ -20,7 +23,24 @@ public class App {
 
         // SimpleTerminal.command(input);
 
-        List<Instrument> instruments = SimpleTerminal.loadInstruments("../债券信息1.csv");
+        /*
+         * - How to get relative path
+         * Path filePath1 = Paths.
+         * get("/Users/justinli/Desktop/Summer Internship/SimpleTradingTerminal/债券信息1.csv"
+         * );
+         * Path filePath2 = Paths.get(
+         * "/Users/justinli/Desktop/Summer Internship/SimpleTradingTerminal/simple-trading-terminal/src/main/java/com/citics/intern/App.java"
+         * );
+         * Path relativePath = filePath2.getParent().relativize(filePath1);
+         * 
+         * // Print the relative path
+         * System.out.println("Relative path from file1 to file2: " + relativePath);
+         */
+        // List<Instrument> instruments = SimpleTerminal
+        // .loadInstruments("../../../../../../../债券信息1.csv");
+
+        List<Instrument> instruments = SimpleTerminal
+                .loadInstruments("./债券信息1.csv");
 
         System.out.println(instruments.get(0));
 
